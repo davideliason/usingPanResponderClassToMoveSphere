cdc49b043b37deaa19931bad769236d1f94336a4
David Eliason <david@davethemaker.com>
2018-06-07 15:50:48 -0700

    create main component WeatherProject

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import WeatherProject from './WeatherProject';
 
-AppRegistry.registerComponent('weatherAppRN', () => App);
+AppRegistry.registerComponent('weatherAppRN', () => WeatherProject);
